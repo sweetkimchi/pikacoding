5c7945707c43c7e39e739758fd2b66546600c085
KATHLEEN CHEN <kc387@duke.edu>
2021-04-04 15:54:17 -0400

    finish setting up control panel (need to set actions to buttons

--- src/ooga/view/level/ControlPanel.java
+++ src/ooga/view/level/ControlPanel.java
@@ -5,13 +5,17 @@ import javafx.scene.control.Button;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.GridPane;
+import javafx.scene.paint.Color;
 import ooga.view.ScreenCreator;
 import java.lang.reflect.Method;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.ResourceBundle;
 
 public class ControlPanel extends GridPane {
   private static final String BUTTON_IMAGES = "ControlPanelButtons";
-  private static final int ICON_SIZE = 20; //TODO: put in properties file
+  private static final String REFLECTION_METHODS = "ControlPanelReflectionActions";
+  private static final int ICON_SIZE = 30; //TODO: put in properties file
   private Object[] buttons;
   private ResourceBundle buttonImages;
 
@@ -19,6 +23,7 @@ public class ControlPanel extends GridPane {
     this.getStyleClass().add("control-panel");
     buttonImages = ResourceBundle.getBundle(ScreenCreator.RESOURCES + BUTTON_IMAGES);
     buttons = buttonImages.keySet().toArray();
+    Arrays.sort(buttons);
     makeButtons();
   }
 
@@ -27,7 +32,7 @@ public class ControlPanel extends GridPane {
     for(Object o: buttons) {
       Button button = new Button();
       button.setGraphic(setIcon(buttonImages.getString(o.toString())));
-//    button.getStyleClass().add(BUTTON);
+      button.getStyleClass().add(o.toString());
 //    button.setId(idsForTesting.getString(key));
       button.setOnAction(event -> reflectionMethod(o.toString()));
       this.add(button, col, 0);
@@ -37,12 +42,30 @@ public class ControlPanel extends GridPane {
 
   private void reflectionMethod(String key) {
     try {
-//      String methodName = reflectionResource.getString(key);
-//      Method m = UserCommandPane.this.getClass().getDeclaredMethod(methodName);
-//      m.invoke(UserCommandPane.this);
+      ResourceBundle reflectionResource = ResourceBundle.getBundle(ScreenCreator.RESOURCES + REFLECTION_METHODS);
+      String methodName = reflectionResource.getString(key);
+      Method m = ControlPanel.this.getClass().getDeclaredMethod(methodName);
+      m.invoke(ControlPanel.this);
     } catch (Exception e) {
       new Alert(Alert.AlertType.ERROR);
     }
+  }
+
+  // TODO: coordinate with playing animation
+  private void play() {
+    System.out.println("Play");
+  }
+
+  private void pause() {
+    System.out.println("Pause");
+  }
+
+  private void step() {
+    System.out.println("Step");
+  }
+
+  private void reset() {
+    System.out.println("Stop and Reset");
   }
 
   private ImageView setIcon(String icon) {
