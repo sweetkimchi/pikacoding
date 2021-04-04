91cf900ee6c88a03fcd08f93dd54916617c44646
Harrison Huang <harrison.huang@duke.edu>
2021-04-04 17:04:57 -0400

    fixed compilation errors

--- src/ooga/model/commands/Commands.java
+++ src/ooga/model/commands/Commands.java
@@ -8,11 +8,11 @@ import ooga.model.player.Avatar;
  */
 public interface Commands {
 
-  /**
-   * Executes the command on an Avatar.
-   *
-   * @param avatar The avatar upon which to execute the command
-   */
-  public void execute(Avatar avatar);
+//  /**
+//   * Executes the command on an Avatar.
+//   *
+//   * @param avatar The avatar upon which to execute the command
+//   */
+//  public void execute(Avatar avatar);
 
 }
