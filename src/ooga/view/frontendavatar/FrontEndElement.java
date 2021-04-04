2af012c6317a3a91cfbaf8fca2e7295732523128
Ji Yun Hyo <jiyun.hyo@duke.edu>
2021-04-04 01:24:34 +0000

    frontendavatar class created

--- src/ooga/view/frontendavatar/FrontEndElement.java
+++ src/ooga/view/frontendavatar/FrontEndElement.java
@@ -2,7 +2,7 @@ package ooga.view.frontendavatar;
 
 import javafx.scene.image.ImageView;
 
-public class FrontEndAvatar extends FrontEndSprite{
+public class FrontEndElement extends FrontEndSprite{
 
   /**
    * Constructor for FrontEndTurtle
@@ -12,7 +12,7 @@ public class FrontEndAvatar extends FrontEndSprite{
    * @param turtleImage image of the turtle
    * @param penState    state of the pen
    */
-  public FrontEndAvatar(double xCoord, double yCoord, ImageView turtleImage,
+  public FrontEndElement(double xCoord, double yCoord, ImageView turtleImage,
       double penState) {
     super(xCoord, yCoord, turtleImage, penState);
   }
