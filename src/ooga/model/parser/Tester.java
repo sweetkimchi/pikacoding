b043ec728251176dd7540a9932b3419aaa9ac78b
Billy Luqiu <wyl6@duke.edu>
2021-04-05 02:38:43 -0400

    testing file for rapid testing of parser

--- src/ooga/model/parser/Tester.java
+++ src/ooga/model/parser/Tester.java
@@ -1,14 +1,16 @@
 package ooga.model.parser;
-//import java.io.File;
-//import java.io.FileReader;
-//import java.net.URL;
-//import java.nio.file.Paths;
-//import java.util.Iterator;
-//import org.json.simple.JSONArray;
-//import org.json.simple.JSONObject;
-//import org.json.simple.parser.JSONParser;
+import java.io.File;
+import java.io.FileReader;
+import java.net.URL;
+import java.nio.file.Paths;
+import java.util.Iterator;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
 
-
+import com.fasterxml.jackson.core.JsonGenerationException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class Tester {
 
