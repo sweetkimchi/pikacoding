6e7f56ff62e696a60d6080601e2c8eb603fc58ec
KATHLEEN CHEN <kc387@duke.edu>
2021-04-14 17:01:22 -0400

    format start screen

--- src/ooga/view/StartMenu.java
+++ src/ooga/view/StartMenu.java
@@ -3,20 +3,29 @@ package ooga.view;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.geometry.Pos;
+import javafx.scene.Group;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.VBox;
 
 public class StartMenu extends BorderPane {
+  private static final String DEFAULT_CSS = ScreenCreator.RESOURCES.replace(".", "/")
+          + "default.css";
 
   public StartMenu(EventHandler<ActionEvent> startAction) {
+    this.getStylesheets().add(DEFAULT_CSS);
+    VBox center = new VBox();
+    center.getStyleClass().add("start-screen");
     Label title = new Label("Title");
-    this.setCenter(title);
+    title.getStyleClass().add("title");
 
     Button startButton = new Button("Start Game");
     startButton.setOnAction(startAction);
-    BorderPane.setAlignment(startButton, Pos.BOTTOM_CENTER);
-    this.setBottom(startButton);
+    //BorderPane.setAlignment(startButton, Pos.BOTTOM_CENTER);
+    center.getChildren().addAll(title, startButton);
+    this.setCenter(center);
   }
 
 }
