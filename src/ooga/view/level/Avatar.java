48938d30e1f5e28beb7ffd552e34437baad1ce9b
KATHLEEN CHEN <kc387@duke.edu>
2021-04-19 00:00:26 -0400

    refactor avatar to not hard code number of images for each animation for each direction

--- src/ooga/view/level/Avatar.java
+++ src/ooga/view/level/Avatar.java
@@ -2,19 +2,22 @@ package ooga.view.level;
 
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import ooga.view.ScreenCreator;
+
+import java.util.ResourceBundle;
 
 public class Avatar {
-
-  private static final String avatarImage = "images/PikachuAvatar.gif"; // TODO: put in resource file or get passed
+  private static final String IMAGE_PROPERTIES = "AnimationImages";
   private int initialXCoordinate;
   private int initialYCoordinate;
   private double width;
   private double height;
   private SpriteLayer spriteLayer;
+  private ResourceBundle animationImages;
   private ImageView avatar;
-  private int i = 0;
-  private int k = 0;
-  private int j = 0;
+  private int right = 0;
+  private int left = 0;
+  private int fdbk = 0;
 
   public Avatar(int x, int y, double w, double h, SpriteLayer root) {
     initialXCoordinate = x;
@@ -22,11 +25,12 @@ public class Avatar {
     width = w;
     height = h;
     spriteLayer = root;
+    animationImages = ResourceBundle.getBundle(ScreenCreator.RESOURCES + IMAGE_PROPERTIES);
     makeAvatar();
   }
 
   private void makeAvatar() {
-    avatar = new ImageView(new Image("images/PikachuAvatar.gif"));
+    avatar = new ImageView(new Image(animationImages.getString("defaultImage") + ".gif"));
     avatar.getStyleClass().add("avatar");
     avatar.setFitWidth(width);
     avatar.setFitHeight(height);
@@ -44,23 +48,21 @@ public class Avatar {
 
     // TODO: refactor
     if(currentX < nextX) {
-      int num = ((i) % 6) + 1;
-      setAvatarImage("images/AnimatedPikachuRight" + num + ".gif");
-      i++;
+      int num = ((right) % Integer.parseInt(animationImages.getString("rightTotal"))) + 1;
+      setAvatarImage(animationImages.getString("rightImage") + num + ".gif");
+      right++;
     }else if(nextX < currentX){
-      int num = ((k) % 6) + 1;
-      setAvatarImage("images/AnimatedPikachuLeft" + num + ".gif");
-      k++;
+      int num = ((left) % Integer.parseInt(animationImages.getString("leftTotal"))) + 1;
+      setAvatarImage(animationImages.getString("leftImage") + num + ".gif");
+      left++;
     }
     else if(nextX == currentX && nextY == currentY){
+      setAvatarImage(animationImages.getString("defaultImage") + ".gif");
 
-//      setAvatarImage("AnimatedPikachuFDBK" + num + ".gif");
-      setAvatarImage("images/PikachuAvatar.gif");
-
-    }else if(nextX == currentX && nextY != currentY){
-      int num = ((j) % 10) + 1;
-      setAvatarImage("images/AnimatedPikachuFDBK" + num + ".gif");
-      j++;
+    } else if(nextX == currentX && nextY != currentY){
+      int num = ((fdbk) % Integer.parseInt(animationImages.getString("fdbkTotal"))) + 1;
+      setAvatarImage(animationImages.getString("fdbkImage") + num + ".gif");
+      fdbk++;
     }
   }
 
