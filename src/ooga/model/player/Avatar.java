68fdf698fc601986f6cccdfdec91405d35d83d03
Harrison Huang <harrison.huang@duke.edu>
2021-04-25 17:40:56 -0400

    added javadoc to avatar/block/datacube

--- src/ooga/model/player/Avatar.java
+++ src/ooga/model/player/Avatar.java
@@ -1,83 +1,105 @@
 package ooga.model.player;
 
 /**
- * 
+ * The Avatar is a type of Player directed by the player of the game able to pick up and drop
+ * blocks.
+ *
+ * @author Harrison Huang
  */
 public class Avatar extends Player {
 
-    private final int id;
-    private Block heldItem;
-    private int xCoord;
-    private int yCoord;
+  private final int id;
+  private Block heldItem;
+  private int xCoord;
+  private int yCoord;
 
-    /**
-     * Default constructor
-     */
-    public Avatar(int id, int xCoord, int yCoord) {
-        this.xCoord = xCoord;
-        this.yCoord = yCoord;
-        this.id = id;
-        heldItem = null;
-    }
+  /**
+   * Default constructor
+   */
+  public Avatar(int id, int xCoord, int yCoord) {
+    this.xCoord = xCoord;
+    this.yCoord = yCoord;
+    this.id = id;
+    heldItem = null;
+  }
 
-    @Override
-    public int getId() {
-        return id;
-    }
+  /**
+   * Getter for the ID of the element.
+   *
+   * @return The ID of the element
+   */
+  @Override
+  public int getId() {
+    return id;
+  }
 
-    /**
-     * @return xCoordinate of the Element
-     */
-    @Override
-    public int getXCoord() {
-        return xCoord;
-    }
+  /**
+   * Getter for the x-coordinate of the Element.
+   *
+   * @return The x-coordinate of the Element
+   */
+  @Override
+  public int getXCoord() {
+    return xCoord;
+  }
 
-    /**
-     * @return yCoordinate of the Element
-     */
-    @Override
-    public int getYCoord() {
-        return yCoord;
-    }
+  /**
+   * Getter for the y-coordinate of the Element.
+   *
+   * @return The y-coordinate of the Element
+   */
+  @Override
+  public int getYCoord() {
+    return yCoord;
+  }
 
-    /**
-     * Updates the X and Y coordinates of the Element.
-     *
-     * @param xCoord The new x-coordinate
-     * @param yCoord The new y-coordinate
-     */
-    @Override
-    public void setXY(int xCoord, int yCoord) {
-        this.xCoord = xCoord;
-        this.yCoord = yCoord;
-    }
+  /**
+   * Updates the X and Y coordinates of the Element.
+   *
+   * @param xCoord The new x-coordinate
+   * @param yCoord The new y-coordinate
+   */
+  @Override
+  public void setXY(int xCoord, int yCoord) {
+    this.xCoord = xCoord;
+    this.yCoord = yCoord;
+  }
 
-    /**
-     * Directs the avatar to pick up a block.
-     *
-     * @param toPickUp The block to pick up
-     */
-    public void pickUp(Block toPickUp) {
-        heldItem = toPickUp;
-    }
+  /**
+   * Directs the avatar to pick up a block.
+   *
+   * @param toPickUp The block to pick up
+   */
+  public void pickUp(Block toPickUp) {
+    heldItem = toPickUp;
+  }
 
-    /**
-     * Directs the avatar to drop the block it is holding. Returns the block it dropped.
-     *
-     * @return The block that was dropped
-     */
-    public Block drop() {
-        Block ret = heldItem;
-        heldItem = null;
-        return ret;
-    }
+  /**
+   * Directs the avatar to drop the block it is holding. Returns the block it dropped.
+   *
+   * @return The block that was dropped
+   */
+  public Block drop() {
+    Block ret = heldItem;
+    heldItem = null;
+    return ret;
+  }
 
-    public boolean hasBlock() {
-        return heldItem != null;
-    }
+  /**
+   * Returns the boolean of whether or not the avatar is holding a block.
+   *
+   * @return The boolean if the avatar has a block
+   */
+  public boolean hasBlock() {
+    return heldItem != null;
+  }
 
-    public Block getHeldItem() {
-        return heldItem;
-    }
+  /**
+   * Returns the block that the avatar is holding.
+   *
+   * @return The block that the avatar is holding
+   */
+  public Block getHeldItem() {
+    return heldItem;
+  }
 }
--- src/ooga/model/player/Block.java
+++ src/ooga/model/player/Block.java
@@ -1,33 +1,48 @@
 package ooga.model.player;
 
 /**
- * 
+ * The Block is a general abstraction for a non-player object in the game. It has an associated
+ * display number and can be set as held or not held.
+ *
+ * @author Harrison Huang
  */
 public abstract class Block implements Element {
 
   private boolean isHeld;
 
   /**
-   * Default constructor
+   * Default constructor of Block. Initialized to be not held.
    */
   public Block() {
     isHeld = false;
   }
 
   /**
-   * Gets the number associated with the
-   * @return
+   * Gets the number associated with the block. To be overridden by the extending classes.
+   *
+   * @return The number associated with the block
    */
   public abstract int getDisplayNum();
 
+  /**
+   * Runs the behavior when the block is dropped. Sets the isHeld boolean to be false.
+   */
   public void drop() {
     isHeld = false;
   }
 
+  /**
+   * Runs the behavior when the block is picked up. Sets the isHeld boolean to be true.
+   */
   public void pickUp() {
     isHeld = true;
   }
 
+  /**
+   * Returns the boolean for whether the block is held or not.
+   *
+   * @return The boolean if the block is held
+   */
   public boolean isHeld() {
     return isHeld;
   }
