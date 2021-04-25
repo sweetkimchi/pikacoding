f4a2bc6a3db9018e971b569e540482297eac6b40
Harrison Huang <harrison.huang@duke.edu>
2021-04-24 20:51:29 -0400

    updated gameProperties JSON for command blocks, timeLimit

--- src/ooga/model/database/DatabaseTester.java
+++ src/ooga/model/database/DatabaseTester.java
@@ -6,7 +6,7 @@ public class DatabaseTester {
 
   public static void main(String[] args) throws IOException, InterruptedException {
     FirebaseService firebaseService = new FirebaseService(0, 0);
-    firebaseService.saveGameLevel(5);
+    firebaseService.saveGameLevel(6);
     //firebaseService.readDBContentsForLevelInit(1);
     //MongoDriver mongoDriver = new MongoDriver();
 
