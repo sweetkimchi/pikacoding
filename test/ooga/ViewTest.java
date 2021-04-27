cec7f7de11309bafd86225717c140469257ce6b9
KATHLEEN CHEN <kc387@duke.edu>
2021-04-27 17:34:21 -0400

    fix logic of view test so it passes pipeline

--- test/ooga/ViewTest.java
+++ test/ooga/ViewTest.java
@@ -58,24 +58,23 @@ class ViewTest extends ApplicationTest {
     assert((stage.getScene().getRoot() instanceof MatchIdSelector));
   }
 
-//  @Test
-//  void testLoadLevel() {
-//    clickButton("start-button");
-//    enterMatchId();
-//    clickButton("single-player-button");
-//    clickButton("load-level-1");
-//    LevelView levelView = screenCreator.getLevelView();
-//    assertEquals(1, (int) getPrivateField(levelView, "level"));
-//  }
-//
-//  @Test
-//  void testLoadMulti() {
-//    clickButton("start-button");
-//    enterMatchId();
-//    clickButton("multiplayer-button");
-//    Stage stage = (Stage) getPrivateField(screenCreator, "stage");
-//    assertTrue(stage.getScene().getRoot() instanceof TeamSelector);
-//  }
+  @Test
+  void testLoadLevel() {
+    clickButton("start-button");
+    enterMatchId();
+    clickButton("single-player-button");
+    clickButton("load-level-1");
+    assertTrue(lookup("#pause-button").query() != null);
+  }
+
+  @Test
+  void testLoadMulti() {
+    clickButton("start-button");
+    enterMatchId();
+    clickButton("multiplayer-button");
+    sleep(1000);
+    assertTrue(lookup("#team1-button").query() != null);
+  }
 
 //  @Test
 //  void successfulSinglePlayerButtonCreation() {
